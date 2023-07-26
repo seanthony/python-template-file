159b4330a5e56d696e3684b70340384fb9f135a3
Sean Anthony <16654808+seanthony@users.noreply.github.com>
2023-07-26 12:48:56 -0500

    Create app.py
    created this file as an easy copy paste for new projects to start with

---
+++ app.py
@@ -0,0 +1,42 @@
+"""
+AUTHOR:     email
+PURPOSE:    text
+DATE:       DD Month YYYY
+"""
+
+
+def new_function():
+    """
+    var -> var
+    
+    returns...
+    
+    description:
+      text
+    """ 
+    logging.info('hello from function')
+    return None
+    
+
+def main():
+    '''
+    
+    '''
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info('hello from main')
+    
+    new_function()
+    return None
+
+    
+if __name__=='__main__':
+    '''
+    why use __name__=='__main__'?
+    
+    code inside this if statement is run when the script is being
+     executed directly by the python interpreter.
+     
+    code inside the if statement will not be executed if the file's
+      code is imported as a module.
+    '''
+    main()
